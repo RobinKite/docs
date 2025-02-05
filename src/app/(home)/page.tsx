17a057277940cb492373f285373800a69b85b577
Alex Bird <horoshiy.ab@gmail.com>
2025-02-06 00:27:18 +0200

    add navbar for main page, template gallery and search

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,13 +1,30 @@
-import Link from "next/link";
+"use client";
+
+import { useQuery } from "convex/react";
+import { Navbar } from "./navbar";
+import { TemplateGallery } from "./template-gallery";
+
+import { api } from "../../../convex/_generated/api";
+import { FullscreenLoader } from "@/components/fullscreen-loader";
 
 const Home = () => {
+  const documents = useQuery(api.documents.get);
+
+  if (documents === undefined) {
+    return <FullscreenLoader label="App loading..." />;
+  }
+
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      Click
-      <Link href="/documents/123">
-        &nbsp;<span className="text-blue-500 underline">here</span>&nbsp;
-      </Link>
-      to go to document id
+    <div className="min-h-screen flex flex-col">
+      <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
+        <Navbar />
+      </div>
+      <div className="mt-16">
+        <TemplateGallery />
+        {documents?.map((document) => (
+          <span key={document._id}>{document.title}</span>
+        ))}
+      </div>
     </div>
   );
 };
